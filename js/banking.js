6a991f7b959c2c6badea2ab6c21595563190ba2b
NU-Faysol <nizamfaysol16@gmail.com>
2022-02-17 15:16:41 +0600

    project done

--- js/banking.js
+++ js/banking.js
@@ -32,4 +32,44 @@ document.getElementById('deposit-button').addEventListener('click', function(){
 
             //clear the deposite input field
         depositInput.value = '';
+});
+
+            //handlewithdraw avent handler
+document.getElementById('withdraw-button').addEventListener('click', function(){
+
+    const withdrawInput = document.getElementById('withdraw-input');
+
+    const withdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withdrawAmountText);
+    
+
+
+            //set withdraw total
+    const withdrawTotal = document.getElementById('withdraw-total');
+
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+
+    withdrawTotal.innerText = newWithdrawTotal;
+
+
+        //update balance
+    const balanceTotal = document.getElementById('balance-total');
+
+    const previousBalanceText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceText);
+
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+
+    newWithdrawAmount.innerText = newBalanceTotal;
+
+
+//clear the withdraw input field
+withdrawInput.value = '';
+
 })
+
+
+
